4f8beb877bca80dca1fa3262cfd358d27fc15dde
Daniil Markov <agushasok@mail.ru>
2024-07-05 14:24:00 +0300

    init app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from "./pages/list/list.component";
+import { ListItemComponent } from "./pages/list-item/list-item.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: ListComponent, pathMatch: 'full' },
+  { path: 'list/:id', component: ListItemComponent },
+  { path: '**', redirectTo: 'list' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './pages/list/list.component';
+import { ListItemComponent } from './pages/list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
