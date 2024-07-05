efff99af00ec81d4689e9cebd21e72d0259eb30d
Daniil Markov <agushasok@mail.ru>
2024-07-05 17:19:20 +0300

    app features testing

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -7,18 +7,13 @@ import { TelegramService } from "../../services/telegram.service";
   styleUrl: './list.component.less'
 })
 export class ListComponent implements OnInit {
-  tgCtx: any;
-
   constructor(
     private readonly telegramService: TelegramService
-  ) {}
+  ) {
+  }
 
   ngOnInit() {
     this.telegramService.tg.ready();
-    setTimeout(() => {
-      this.tgCtx = this.telegramService.tg;
-      this.telegramService.tg.MainButton.show();
-      this.telegramService.tg.SettingsButton.hide();
-    }, 0);
+    this.telegramService.tg.MainButton.show();
   }
 }
