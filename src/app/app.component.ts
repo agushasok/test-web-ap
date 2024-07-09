6653b39bfa65f9d2725fd86b730e417e7699547f
Daniil Markov <agushasok@mail.ru>
2024-07-09 14:35:09 +0300

    routing fix #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit } from '@angular/core';
 import { TelegramService } from "./services/telegram.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
-    private readonly telegramService: TelegramService
+    private readonly telegramService: TelegramService,
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+    private readonly destroyRef: DestroyRef
   ) {
     this.telegramService.tg.ready();
+  }
+
+  ngOnInit() {
+    this.route.queryParams
+      .pipe(
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe(p => {
+        if (p['path'] != null) {
+          this.router.navigate([p['path']])
+        }
+      })
   }
 }
